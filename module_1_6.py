0845bd57745577ca7683da1fd86ab0bfa7a7339a
ljusien <ljusien777@gmail.com>
2024-11-29 15:15:47 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,10 @@
+# first_name = 123
+# second_name = 456
+# third_name = 789
+# if first_name == second_name == third_name:
+# print(first_name, second_name, third_name)
+x = 42
+y = 69
+z = 42
+if (42==69 or 42==42):
+    print(x,y,z)
